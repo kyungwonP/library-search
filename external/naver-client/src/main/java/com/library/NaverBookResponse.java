c2d563fb91b2eefe949088064bfa8bc007e5aa47
parkkyungwon <kywonp@gmail.com>
2025-04-02 19:26:42 +0900

    [LS-4] 외부 api 연동 - 네이버 응답 형식 적용

---
+++ external/naver-client/src/main/java/com/library/NaverBookResponse.java
@@ -0,0 +1,11 @@
+package com.library;
+
+import java.util.List;
+
+public class NaverBookResponse {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private List<Item> items;
+}
